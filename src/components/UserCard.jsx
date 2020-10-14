31d6c09e00bafe7adac83491edcd6f2ba4431288
Dylan Cooper <dylancooper155@gmail.com>
2020-10-14 13:53:47 -0400

    pulling in user profile pictures

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import UserCardList from "./UserCardList";
-
-
-
 import ReactMarkdown from "react-markdown";
 import { useParams } from "react-router-dom";
 
 const UserCard = (props) => {
+    return (
 
+        
+    )
 }
 
 
--- src/components/UserCardList.jsx
+++ src/components/UserCardList.jsx
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Route, Link } from "react-router-dom";
 import { loadData } from "../utils/loadData";
+import { Card } from 'bloomer';
+import userEvent from "@testing-library/user-event";
 
 
 const UserCardList = (props) => {
-    
+    const { user } = props
+    return (
+       <>
+        
+        <Card>
+        <img src={user.avatar_url}/>
+
+        </Card>
+
+        </>
+
+    )
 };
 
 export default UserCardList;
